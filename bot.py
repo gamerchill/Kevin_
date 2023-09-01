0620a0e0a453ca76816008c64f3a2c5a095f68de
Chill <98439157+gamerchill@users.noreply.github.com>
2023-08-31 23:14:16 -0700

    rehauled the discord code, renamed to bot.py

---
+++ bot.py
@@ -0,0 +1,78 @@
+import asyncio
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
+
+class SupremeHelpCommand(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return '%s%s %s' % (self.context.clean_prefix, command.qualified_name, command.signature)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", color=discord.Color.blurple())
+        for cog, commands in mapping.items():
+            filtered = await self.filter_commands(commands, sort=True)
+            if command_signatures := [
+                self.get_command_signature(c) for c in filtered
+            ]:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=self.get_command_signature(command) , color=discord.Color.blurple())
+        if command.help:
+            embed.description = command.help
+        if alias := command.aliases:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_help_embed(self, title, description, commands): # a helper function to add commands to an embed
+        embed = discord.Embed(title=title, description=description or "No help found...")
+
+        if filtered_commands := await self.filter_commands(commands):
+            for command in filtered_commands:
+                embed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+
+        await self.get_destination().send(embed=embed)
+
+    async def send_group_help(self, group):
+        title = self.get_command_signature(group)
+        await self.send_help_embed(title, group.help, group.commands)
+
+    async def send_cog_help(self, cog):
+        title = cog.qualified_name or "No"
+        await self.send_help_embed(f'{title} Category', cog.description, cog.get_commands())
+
+bot.help_command = SupremeHelpCommand()
+
+
+
+@bot.hybrid_command(name="first_slash")
+async def first_slash(ctx): 
+   await ctx.send("You executed the slash command!") #respond no longer works, so i changed it to send
+
+@bot.event
+async def on_ready():
+   await bot.sync() #sync the command tree
+   print("Bot is ready and online")
+
+
+
+
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+async def main():
+    await load()
+    await bot.start('MTA0MjMyMjkxMTQ4ODM3NjkxMg.GveLnr.pw5IDGwV6YSa0xKbziLHbyZEY8IYxA03Vs9mNs')
+
+asyncio.run(main())
